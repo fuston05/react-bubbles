9f303e5392748cde6558edec0e14357552e64563
fuston05 <fuston05@yahoo.com>
2020-02-28 13:59:08 -0500

    couldnt get stretch in time...

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
-import {axiosWithAuth} from '../utils/axiosWithAuth';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const initialColor = {
   color: "",
-  code: { hex: "" }
+  code: { hex: "" },
+  id: Date.now()*Math.random()
 };
 
 const ColorList = ({ colors, updateColors }) => {
   console.log('colors', colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd]= useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -23,15 +25,15 @@ const ColorList = ({ colors, updateColors }) => {
     axiosWithAuth()
       .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
       .then(putRes => {
-        let newColors= colors.map(color => {
-          if(color.id === putRes.data.id){
-            return color= putRes.data
-           
-          }else{return color}
+        let newColors = colors.map(color => {
+          if (color.id === putRes.data.id) {
+            return color = putRes.data
+          } else { return color }
         })
         updateColors([
           ...newColors
         ])
+        setColorToEdit(initialColor);
         console.log(putRes.data);
       })
       .catch(putErr => {
@@ -46,7 +48,7 @@ const ColorList = ({ colors, updateColors }) => {
     axiosWithAuth()
       .delete(`/api/colors/${color.id}`)
       .then(delRes => {
-        let newColors= colors.filter(color => {
+        let newColors = colors.filter(color => {
           return parseInt(color.id) !== parseInt(delRes.data)
         });
         console.log('newColors', newColors)
@@ -55,9 +57,23 @@ const ColorList = ({ colors, updateColors }) => {
         ])
         console.log('delRes.data: ', delRes.data);
       })
-      .catch(delErr => {console.log(delErr);})
-
+      .catch(delErr => { console.log(delErr); })
   }//end deleteColor
+
+  const addNewColor= (e, colorToAdd) => {
+    e.preventDefault();
+    console.log('addcolor: ', colorToAdd);
+    axiosWithAuth()
+      .post('/api/colors', colorToAdd)
+      .then(addRes => {
+        updateColors([
+          ...colors,
+          addRes.data
+        ])
+        console.log('addRes', addRes);
+      })
+      .catch(addErr => {console.log(addErr);})
+  }//end addNewColor
 
   return (
     <div className="colors-wrap">
@@ -67,11 +83,11 @@ const ColorList = ({ colors, updateColors }) => {
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+                e.stopPropagation();
+                deleteColor(color)
+              }
+              }>
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -114,7 +130,33 @@ const ColorList = ({ colors, updateColors }) => {
       )}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
+      <form onSubmit= {(e) => addNewColor(e, colorToAdd)}>
+        <label htmlFor= 'colorName'>Color Name</label>
+        <input 
+          value= {colorToAdd.color}
+          onChange= {(e) => setColorToAdd({
+            ...colorToAdd,
+            color: e.target.value
+          })}
+          type= 'text'
+          name= 'colorName'
+          id= 'colorName'
+          placeholder= 'Color Name'
+        />
+        <label htmlFor= 'hex'>Color Hex</label>
+        <input 
+          value= {colorToAdd.hex}
+          
+          type= 'text'
+          name= 'hex'
+          id= 'hex'
+          placeholder= 'Hex Value'
+        />
+        <button type= 'submit'>Add Color</button>
+      </form>
+      {console.log('colorToAdd: ', colorToAdd)}
     </div>
+    
   );
 };
 
